04ca75f9ab694b92538cae22e26474db504beb68
volps <volpatto@lncc.br>
2018-01-22 02:27:29 -0200

    Add a 2D experiment with SUPG stabilization

---
+++ exp2DM3SUPG/quadm3.d
@@ -0,0 +1,28 @@
+#-----------#
+# Unit quad case #
+#-----------#
+
+#=========
+| POINTS |
+=========#
+5 # number of points #
+
+# Nodes which define the boundary #
+0:   0   0  0.1    1
+1:   1   0  0.1    1
+2:   1   1  0.1    1
+3:   0   1  0.1    1
+
+# Material markers #
+4: 0.5   0.5    0     1 
+
+#===========
+| SEGMENTS |
+===========#
+4 # Number of segments #
+
+# Boundary segments #
+0:   0   1   1
+1:   1   2   1
+2:   2   3   1
+3:   3   0   1
