8e46d729be8a4c2d792c73a5656c584d35b80212
volps <volpatto@lncc.br>
2018-01-22 02:25:38 -0200

    [issue] Obsolete documentation updating

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['analyzefile',['analyzefile',['../namespaceminputreader.html#a8e123ff1256a30a53b264785d019851e',1,'minputreader']]],
-  ['analyzefileinput',['analyzefileinput',['../namespaceminputreader.html#a21755e83e62475cff7e1fd0bb7530fd8',1,'minputreader']]],
-  ['applybc',['applybc',['../namespacemprocessor.html#a02f899df532bd1f0fa81a79e079b4e93',1,'mprocessor']]],
-  ['assmb',['assmb',['../namespacemprocessor.html#a2b08d50f9aac352fcb83bdb8b1d96922',1,'mprocessor']]]
+  ['finel_20_2d_2d_20a_20general_20purpose_20finite_20element_20code',['FINEL -- A general purpose FINite ELement code',['../md_README.html',1,'']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,30 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acdefgiklmnopqrstuvwxyz",
-  1: "ms",
-  2: "ms",
-  3: "dimpstu",
-  4: "acdfgilmnopqrstz",
-  5: "defgiklmnoqrstuvwxyz"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables"
 };
 
